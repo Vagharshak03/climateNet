45cc660847a8596dbf9439953002ad77b40578b0
Ubuntu <ubuntu@ip-172-31-46-105.ec2.internal>
2024-11-02 19:57:57 +0000

    flask app

---
+++ flask_app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def first_page():
+    button = "<a href='/next_page' target='_blank'><button type='button'>Next page</button>"
+    return button
+
+@app.route('/next_page')
+def second_page():
+    button = "<a href='/' target='_blank'><button type='button'>Previous page</button>"
+    return button
+
+if __name__ == '__main__':
+    app.run(port='5000', debug=True)
+
