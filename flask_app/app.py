ca8c70813ff1080d47ee1264944123ee6b81a07e
Ubuntu <ubuntu@ip-172-31-46-105.ec2.internal>
2024-11-19 21:36:37 +0000

    small changes

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,16 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/prev_page')
 def first_page():
     button = "<a href='/next_page' target='_blank'><button type='button'>Next page</button>"
     return button
 
 @app.route('/next_page')
 def second_page():
-    button = "<a href='/' target='_blank'><button type='button'>Previous page</button>"
+    button = "<a href='/prev_page' target='_blank'><button type='button'>Previous page</button>"
     return button
 
+@app.route('/test')
+def test():
+    return ''
+
+
 if __name__ == '__main__':
-    app.run(port='5000', debug=True)
+    app.run(host='0.0.0.0', port='8484', debug=True)
 
